d022254a59107ffeb1b0350e20ee6cb972566b6b
John <john-francis.edah@tech11.com>
2024-08-31 04:02:43 +0200

    added src file structure ao app

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontWeight: 'bold', letterSpacing: '0.05rem', fontSize: '3rem'}}>PockID</Text> 
+      <br></br>
+      <Text>Pocket Your IDs, Unlock Your Freedom!</Text>
       <StatusBar style="auto" />
     </View>
   );
